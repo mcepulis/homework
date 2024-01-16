7c20bc5aa562bd0bc1cf4067602e49e9525dcc09
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-01-16 21:46:33 +0200

    nd2 sprendimai

--- class/nd2.js
+++ class/nd2.js
@@ -53,4 +53,30 @@
     console.log(evenNumbers);
 
 
+    const evenNumbers2 = arrayNumber.filter(x => x % 2 === 0);
+    console.log(evenNumbers2);
 
+
+   // Apjungimas: Apjunkite du masyvus į vieną.
+
+   const array1 = [1,2,3,4,5];
+   const array2 = [6,7,8,9,10];
+
+   const arraySum = array1.concat(array2);
+   console.log(arraySum);
+
+   /* Elemento pakartojimas: Pakartokite kiekvieną masyvo elementą tris kartus ir saugokite 
+   rezultatą naujame masyve. */
+
+   const trippleArray = Array(3).fill(arrayNumber).flat();
+   console.log(trippleArray);
+
+   //Maksimalus skaičius: Raskite didžiausią skaičių savo masyve
+
+    // const maxNumber = arrayNumber.map(x) => Math.max(x);
+    const maxNumber = Math.max(...arrayNumber);
+    console.log(`array ${[arrayNumber]} maxNumber is ${maxNumber}`);
+
+    //Minimalus skaičius: Raskite mažiausią skaičių savo masyve. 
+    const minNumber = Math.min(...arrayNumber);
+    console.log(`array ${[arrayNumber]} maxNumber is ${minNumber}`);
