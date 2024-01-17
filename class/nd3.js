3028ce2a8821dfaded0b49daa093e83d70848bdd
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-01-17 16:50:55 +0200

    naujo uzduotys

---
+++ class/nd3.js
@@ -0,0 +1,75 @@
+//_1. Sukurkite funkciją kuri priimtų parametrą "name". Jos tikslas atspausdinti naršyklėje tekstą 
+//"Labas," ir šalia jo perduotą vardą. Pvz "Labas, Elena".
+
+
+
+
+
+//_2. Sukurkite funkciją  kuri priimtų vieną parametrą (tekstą) ir grąžintų atsakymą kiek simbolių 
+//yra tame tekste. Iš funkcijos gautą rezultatą atvaizduokite naršyklėje.
+
+
+
+
+
+//_3. Sukurkite funkciją kuri priimtų du parametrus "name" ir "last_name". Funkcija turi grąžinti 
+//vieną stringą, kuriame būtų vardas ir pavardė prasidedantys didžiosiomis raidėmis.
+
+
+
+
+
+
+//_4. Parašykite funkciją kuri sugeneruotų 3 random skaičius nuo 0 iki 5 ir atspausdintų konsolėje 
+//vienoje eilutėje atskirtus kableliais. Po paskutinio skaičiaus kablelio neturi būti.
+
+
+
+
+
+
+
+//_5. Parašykite funkciją kuri priimtų tris parametrus "from" ir "to" ir "limit". Patikrinkite ar šie 
+//perduodami parametrai yra skaičiai ir pagal juos sugeneruokite masyvą, kurio ilgį nusako "limit" 
+//parametras, o reikšmės from ir to nurodo atsitiktinį skaičių šiuose rėžiuose.
+
+
+
+
+//_6. Sukurkite funkciją kuri pakeltų paduotą skaičių n laipsniu, ir grąžintų reikšmę 
+//(perduodami du parametrai: skaičius ir laipsnis)
+
+
+
+
+
+
+//_7. Parašykite funkciją, kurios parametras būtų tekstas, kuris yra įterpiamas į h1 tagą. 
+//Rezultatą atvaizduokite naršyklėje.
+
+
+
+
+
+//_8. Parašykite funkciją su dviem parametrais. Pirmasis yra tekstas, įterpiamas į h tagą, 
+//o antrasis tago numeris (1-6). Rašydami šią funkciją remkitės antrame uždavinyje parašyta funkcija.
+
+
+
+
+
+
+//_9. Aprašykite funkciją kuri priimtų vieną parametrą "number" bei patikrintų ar duotas skaičius 
+//yra pirminis. Atgal grąžinkite teigiamą arba neigiamą (boolean) atsakymą.
+// https://lt.wikipedia.org/wiki/Pirminis_skai%C4%8Dius
+
+
+
+
+//_10. Sugeneruokite 100 elementų masyvą kurio reikšmės atsitiktiniai skaičiai nuo 997 iki 15991. 
+
+
+
+
+//_11. Pasinaudodami devintoje užduotyje aprašyta funkcija masyve palikite tik pirminius skaičius, 
+//kurie yra didesni nei 5000;
